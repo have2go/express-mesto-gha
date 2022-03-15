ae3273beab41d7a9f58b6175050137b6dc678017
have2go <hypercrock@yandex.ru>
2022-03-15 11:06:49 +0300

    Добавил обработку ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -11,13 +11,28 @@ module.exports.createCard = (req, res) => {
 
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(
+      (err) => {
+        if (err.name === 'ValidationError') {
+          res.status(400).send({ message: err.message });
+        } else {
+          res.status(500).send({ message: err.message });
+        }
+      },
+    );
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.user._id)
+    .orFail(new Error('NotValid'))
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.message === 'NotValid') {
+        res.status(404).send({ message: 'Нет карточки с таким id' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
 module.exports.likeCard = (req, res) => {
@@ -26,8 +41,15 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true }, // передеть обновленный объект в then
   )
+    .orFail(new Error('NotValid'))
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.message === 'NotValid') {
+        res.status(404).send({ message: 'Нет карточки с таким id' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -36,6 +58,13 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true }, // передеть обновленный объект в then
   )
+    .orFail(new Error('NotValid'))
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.message === 'NotValid') {
+        res.status(404).send({ message: 'Нет карточки с таким id' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -5,19 +5,34 @@ module.exports.createUser = (req, res) => {
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(400).send({ message: err.message }));
+    .catch(
+      (err) => {
+        if (err.name === 'ValidationError') {
+          res.status(400).send({ message: err.message });
+        } else {
+          res.status(500).send({ message: err.message });
+        }
+      },
+    );
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.user._id)
+    .orFail(new Error('NotValid'))
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(400).send({ message: err.message }));
+    .catch((err) => {
+      if (err.message === 'NotValid') {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
 module.exports.getAllUsers = (req, res) => {
   User.find({})
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(400).send({ message: err.message }));
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.updateUser = (req, res) => {
@@ -34,8 +49,17 @@ module.exports.updateUser = (req, res) => {
       runValidators: true, // данные будут валидированы перед изменением
     },
   )
+    .orFail(new Error('NotValid'))
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(400).send({ message: err.message }));
+    .catch((err) => {
+      if (err.message === 'NotValid') {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+      } else if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
 module.exports.updateUserAvatar = (req, res) => {
@@ -51,6 +75,15 @@ module.exports.updateUserAvatar = (req, res) => {
       runValidators: true, // данные будут валидированы перед изменением
     },
   )
+    .orFail(new Error('NotValid'))
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(400).send({ message: err.message }));
+    .catch((err) => {
+      if (err.message === 'NotValid') {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+      } else if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
